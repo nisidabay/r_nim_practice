217b449a3239e92236483341ded18c4d15804d77
nisidabay <nisidabay@gmail.com>
2026-06-27 21:38:07 +0200

    feat: modify 02_control_flow/exercises/ex03_fibonacci.nim

--- 02_control_flow/exercises/ex03_fibonacci.nim
+++ 02_control_flow/exercises/ex03_fibonacci.nim
@@ -1,13 +1,15 @@
 # Exercise 3: Fibonacci Iterator
 # Custom iterator that yields Fibonacci numbers up to a limit.
-iterator fibonacci(limit: int): int =
-  var a = 0
-  var b = 1
+iterator fibonacci(limit: int64): int64 {.inline.} =
+  var a = 0'i64
+  var b = 1'i64
+
   while a <= limit:
     yield a
-    let temp = a + b
+    let next = a + b
     a = b
-    b = temp
+    b = next
 
-for n in fibonacci(100):
+for n in fibonacci(1000):
   echo n
+
