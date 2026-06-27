852fec6059d0d742252869f36df6e14959dc92eb
nisidabay <nisidabay@gmail.com>
2026-06-27 21:38:53 +0200

    feat: modify 03_procedures/exercises/ex02_palindrome.nim

--- 03_procedures/exercises/ex02_palindrome.nim
+++ 03_procedures/exercises/ex02_palindrome.nim
@@ -1,10 +1,24 @@
 # Exercise 2: Palindrome Checker
-import std/strutils
+import std/[os, strformat]
+
+let
+  progName = paramStr(0)
+  progArgsCount = paramCount()
+
+if progArgsCount < 1 or progArgsCount > 1:
+  echo fmt"Usage: {progName} <word>"
+  quit(1)
+
+let word = paramStr(1)
 
 proc reverse(s: string): string =
   result = ""
   for i in 0 ..< s.len:
     result = s[i] & result
+    echo result
 
-echo "racecar".reverse()
-echo "racecar" == "racecar".reverse()   # true
+if word == word.reverse():
+  echo "This word is a Palindrome"
+else:
+  echo "This word is not a Palindrome"
+
